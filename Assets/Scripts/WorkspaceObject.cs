69686d230742f98024c59e47fee03aa60183f5a7
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-05-03 08:08:40 +0200

    add workspaceObject

---
+++ Assets/Scripts/WorkspaceObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WorkspaceObject : MonoBehaviour
+{
+    [field: SerializeField]
+    public string id { get; private set; }
+}
