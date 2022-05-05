82bdfeb5dda6a7257eb9f0e90dfc8261a77b73e6
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-05-05 09:53:20 +0200

    fix namespace

--- Assets/Scripts/WorkspaceObject.cs
+++ Assets/Scripts/WorkspaceObject.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class WorkspaceObject : MonoBehaviour
+namespace pladdra_app.Assets.Scripts
 {
-    [field: SerializeField]
-    public string id { get; private set; }
+    public class WorkspaceObject : MonoBehaviour
+    {
+        [field: SerializeField]
+        public string id { get; private set; }
+    }
 }
