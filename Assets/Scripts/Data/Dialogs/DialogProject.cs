8334ab052e1e4029a3f390de95e31468bcd99814
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-05-05 09:46:21 +0200

    add dialog workspace item

--- Assets/Scripts/Data/Dialogs/DialogProject.cs
+++ Assets/Scripts/Data/Dialogs/DialogProject.cs
@@ -1,13 +1,25 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace pladdra_app.Assets.Scripts.Data.Dialogs
 {
-    public class DialogResource {
+    public class DialogWorkspaceItem
+    {
+        public string id;
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 scale;
+        public DialogResource resource;
+    }
+
+    public class DialogResource
+    {
         public string Id { get; set; }
         public string Type { get; set; }
         public string Url { get; set; }
     }
-    public class DialogProject {
+    public class DialogProject
+    {
         public string Id { get; set; }
 
         public List<DialogResource> Resources { get; set; }
