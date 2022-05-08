aa4fe7b0d647ad29bcc931df0e27b089e8b00b1b
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-05-08 18:07:44 +0200

    add before & after activation actions

--- Assets/Scripts/Pipelines/Pipeline.cs
+++ Assets/Scripts/Pipelines/Pipeline.cs
@@ -4,26 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using pladdra_app.Assets.Scripts.Data;
 using pladdra_app.Assets.Scripts.Data.Dialogs;
+using pladdra_app.Assets.Scripts.Workspace;
 using UnityEngine;
 
 namespace pladdra_app.Assets.Scripts.Pipelines
 {
-    public class Pipeline {
+    public class Pipeline
+    {
         public Func<IDialogProjectRepository> CreateDialogProjectRepository { get; set; }
-        
+
         public Func<WebResourceManager> CreateWebResourceManager { get; set; }
 
-        public Func<DialogResource, GameObject, IPladdraWorkspaceItem> CreateWorkspaceItem {get; set; }
+        public Func<DialogResource, GameObject, IWorkspaceResource> CreateWorkspaceResource { get; set; }
 
-        public Func<DialogProject, IEnumerable<IPladdraWorkspaceItem>, IPladdraWorkspace> CreateWorkspace {get; set; }
+        public Func<DialogProject, IEnumerable<IWorkspaceResource>, IWorkspaceResourceCollection> CreateWorkspaceResourceCollection { get; set; }
 
-        public Pipeline () {
+        public Pipeline()
+        {
             CreateDialogProjectRepository = () => new SampleDialogProjectRepository(Application.temporaryCachePath);
             CreateWebResourceManager = () => new WebResourceManager(Application.temporaryCachePath);
-            CreateWorkspaceItem = (DialogResource resource, GameObject prefab) => new PladdraWorkspaceItem{Prefab = prefab}; 
-            CreateWorkspace = (DialogProject project, IEnumerable<IPladdraWorkspaceItem> items) => new PladdraWorkspace{Items = items};
+            CreateWorkspaceResource = (DialogResource resource, GameObject prefab) => new WorkspaceResource { prefab = prefab, resourceID = resource.Url };
+            CreateWorkspaceResourceCollection = (DialogProject project, IEnumerable<IWorkspaceResource> items) => new WorkspaceResourceCollection { resources = items };
         }
-        public IEnumerator LoadWorkspace (Action<IPladdraWorkspace> callback) {
+        public IEnumerator LoadWorkspace(Action<IWorkspaceResourceCollection> callback)
+        {
             Debug.Log("[pipline] loading external project...");
             var repo = CreateDialogProjectRepository();
             DialogProject project = null;
@@ -38,8 +42,10 @@ namespace pladdra_app.Assets.Scripts.Pipelines
 
             Debug.Log("[pipline] loading 3d model prefabs...");
             var path2model = new Dictionary<string, GameObject>();
-            foreach(var path in url2path.Values) {
-                yield return new Load3dModel(path, go => {
+            foreach (var path in url2path.Values)
+            {
+                yield return new Load3dModel(path, go =>
+                {
                     Debug.Log($"[pipline] done loading 3d model {path}");
                     path2model[path] = go;
                 });
@@ -49,27 +55,26 @@ namespace pladdra_app.Assets.Scripts.Pipelines
             Debug.Log("[pipline] creating workspace items...");
             var modelItems = project.Resources
                     .Where(resource => resource.Type == "model")
-                    .Select(resource => new {resource, gameObject = path2model.TryGet(url2path.TryGet(resource.Url))})
+                    .Select(resource => new { resource, gameObject = path2model.TryGet(url2path.TryGet(resource.Url)) })
                     .Where(o => o.gameObject != null)
-                    .Select(o => CreateWorkspaceItem(o.resource, o.gameObject))
+                    .Select(o => CreateWorkspaceResource(o.resource, o.gameObject))
                     .Where(item => item != null)
                     .ToList();
 
             var markerItems = project.Resources
                     .Where(resource => resource.Type == "marker")
-                    .Select(resource => new {resource, gameObject = path2model.TryGet(url2path.TryGet(resource.Url))})
+                    .Select(resource => new { resource, gameObject = path2model.TryGet(url2path.TryGet(resource.Url)) })
                     .Where(o => o.gameObject != null)
-                    .Select(o => CreateWorkspaceItem(o.resource, o.gameObject))
+                    .Select(o => CreateWorkspaceResource(o.resource, o.gameObject))
                     .Where(item => item != null)
                     .ToList();
             Debug.Log("[pipline] done creating workspace items");
 
             Debug.Log("[pipline] creating workspace...");
-            var ws = CreateWorkspace(project, modelItems.Concat(markerItems)
+            var ws = CreateWorkspaceResourceCollection(project, modelItems.Concat(markerItems)
                     .ToList());
-            
+
             callback(ws);
         }
-
     }
 }
--- Assets/Scripts/Screen.cs
+++ Assets/Scripts/Screen.cs
@@ -2,27 +2,56 @@ using UnityEngine;
 
 namespace pladdra_app.Assets.Scripts
 {
-    public abstract class Screen: MonoBehaviour {
+    public abstract class Screen : MonoBehaviour
+    {
         virtual public object[] GetScreenChildObjects() { return new object[0]; }
-        public void SetScreenActive (bool active) {
+
+        virtual protected void BeforeActivateScreen() { }
+        virtual protected void AfterActivateScreen() { }
+        virtual protected void BeforeDeactivateScreen() { }
+        virtual protected void AfterDeactivateScreen() { }
+
+
+        public void SetScreenActive(bool active)
+        {
+            if (active)
+            {
+                BeforeActivateScreen();
+            }
+            else
+            {
+                BeforeDeactivateScreen();
+            }
             gameObject.SetActive(active);
 
-            foreach(var child in GetScreenChildObjects()) {
+            foreach (var child in GetScreenChildObjects())
+            {
                 var childScreen = child as Screen;
-                if (childScreen) {
+                if (childScreen)
+                {
                     childScreen.SetScreenActive(active);
                     continue;
                 }
                 var childMonoBehaviour = child as MonoBehaviour;
-                if (childMonoBehaviour) {
+                if (childMonoBehaviour)
+                {
                     childMonoBehaviour.gameObject.SetActive(active);
                     continue;
                 }
                 var childGameObject = child as GameObject;
-                if (childGameObject) {
+                if (childGameObject)
+                {
                     childGameObject.SetActive(active);
                     continue;
                 }
+            }
+            if (active)
+            {
+                AfterActivateScreen();
+            }
+            else
+            {
+                AfterDeactivateScreen();
             }
         }
     }
