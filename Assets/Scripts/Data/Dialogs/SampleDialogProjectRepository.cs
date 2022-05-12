159ff6f80c01037b923501e7fd6bb40c0e5ecdc5
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-05-12 20:08:20 +0200

    update mock models

--- Assets/Scripts/Data/Dialogs/SampleDialogProjectRepository.cs
+++ Assets/Scripts/Data/Dialogs/SampleDialogProjectRepository.cs
@@ -7,8 +7,11 @@ namespace Data.Dialogs
         
         static string[] SampleModels = new string[] {
             // "https://modul-test.helsingborg.io/wp-content/uploads/sites/30/2022/03/telefonare.glb",
-            "https://modul-test.helsingborg.io/wp-content/uploads/sites/30/2022/03/lego_test.glb",
-            "https://modul-test.helsingborg.io/wp-content/uploads/sites/30/2022/02/svensgardsskolan_4.glb"
+            //"https://modul-test.helsingborg.io/wp-content/uploads/sites/30/2022/03/lego_test.glb",
+            //"https://modul-test.helsingborg.io/wp-content/uploads/sites/30/2022/02/svensgardsskolan_4.glb"
+            "https://helsingborgs-rummet.s3.eu-north-1.amazonaws.com/ballong.glb",
+            "https://modul-test.helsingborg.io/wp-content/uploads/sites/30/2022/05/7-museum-maria-park.glb",
+            "https://modul-test.helsingborg.io/wp-content/uploads/sites/30/2022/05/20-lekplats-1.glb",
         };
 
         public SampleDialogProjectRepository(string tempPath)
